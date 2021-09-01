eace28483c31d58452a2d09b1f6ffdf834bfda34
gkolman <gkolman17@gmail.com>
2021-09-01 16:37:00 -0700

    add test

--- src/test/java/com/test/seleniumTest/SeleniumTest.java
+++ src/test/java/com/test/seleniumTest/SeleniumTest.java
@@ -19,50 +19,47 @@ import org.openqa.selenium.JavascriptExecutor;
 import java.net.URL;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-//import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
-	@Test
-    public void print() {
-	  	  System.out.println("success -> running selenium test ");
-	    }
-  public static final String AUTOMATE_USERNAME = "gageguzman_ZaV8mo";
-  public static final String AUTOMATE_ACCESS_KEY = "vpJhwMFZX3XKUPseGmWu";
-  public static final String URL = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
-  public static void main(String[] args) throws Exception {
+	WebDriver driver;
+	@BeforeClass
+	public void testSetup()
+	{
+	System.setProperty("webdriver.chrome.driver", "/users/roberto/documents/chromedriver");
+	driver=new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	driver.manage().window().maximize();
+	}
 
+	@BeforeMethod
+	public void openBrowser()
+	{
+	driver.get("https://www.browserstack.com/");
+	driver.findElement(By.id("signupModalButton")).click();
+	System.out.println("We are currently on the following URL" +driver.getCurrentUrl());
+	}
 
-	    DesiredCapabilities caps = new DesiredCapabilities();
-	    caps.setCapability("browserstack.debug", "true");
-	    caps.setCapability("os_version", "Catalina");
-	    caps.setCapability("resolution", "1920x1080");
-	    caps.setCapability("browser", "Chrome");
-	    caps.setCapability("browser_version", "latest");
-	    caps.setCapability("os", "OS X");
-	    caps.setCapability("name", "BStack-[Java] Sample Test"); // test name
-	    caps.setCapability("build", "BStack Build Number 1"); // CI/CD job or build name
-	    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
-	    driver.get("https://www.google.com");
-	    WebElement element = driver.findElement(By.name("q"));
-	    element.sendKeys("BrowserStack");
-	    element.submit();
-	    // Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page contains 'BrowserStack'
-	    WebDriverWait wait = new WebDriverWait(driver, 5);
-	    try {
-	    	wait.until(ExpectedConditions.titleContains("BrowserStack"));
-	    	markTestStatus("passed","Yaay title contains 'BrowserStack'!",driver);
-	    }
-	    catch(Exception e) {
-	    	markTestStatus("failed","Naay title does not contain 'BrowserStack'!",driver);
-	    }
-	    System.out.println(driver.getTitle());
-	    driver.quit();
-	  }
+	@Test(description="This method validates the sign up functionality")
+	public void signUp()
+	{
+	driver.findElement(By.id("user_full_name")).sendKeys("Sadhvi Singh");
+	driver.findElement(By.id("user_email_login")).sendKeys("sadhvisingh9049+1@gmail.com");
+	driver.findElement(By.id("user_password")).sendKeys("BrowserStack123*");
+	driver.findElement(By.xpath("//input[@name='terms_and_conditions']")).click();
+	driver.findElement(By.id("user_submit")).click();
+	}
 
-	  // This method accepts the status, reason and WebDriver instance and marks the test on BrowserStack
-	  public static void markTestStatus(String status, String reason, WebDriver driver) {
-		JavascriptExecutor jse = (JavascriptExecutor)driver;
-		jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \""+status+"\", \"reason\": \""+reason+"\"}}");
-	  }
+	@AfterMethod
+	public void postSignUp()
+	{
+	System.out.println(driver.getCurrentUrl());
+
+	}
+	@AfterClass
+	public void afterClass()
+	{
+	driver.quit();
+	}
 }
